89f19d57409be5a4f72f52c9720a0a60eb1910b5
Yejin Jang <jyjin0428@gmail.com>
2025-10-17 18:21:35 +0900

    refactor: 랜덤 레이아웃 송출

--- src/main.tsx
+++ src/main.tsx
@@ -2,14 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
-import LearningLayout from './layouts/LearningLayout';
+import RandomLearningLayout from './layouts/RandomLearningLayout';
 import LearningStepPage from './pages/LearningStepPage';
 import GameClearPage from './pages/GameClearPage';
 import LearningIntroPage from './pages/LearningIntroPage';
-import GameLayout from './layouts/GameLayout';
-import GameStepPage from './pages/GameStepPage';
-import GameResultPage from './pages/GameResultPage';
-import GameIntroPage from './pages/GameIntroPage';
 import Home from './pages/Home';
 import LearningStatusPage from './pages/LearningStatusPage';
 import MainLayout from './components/layout/MainLayout';
@@ -44,8 +40,7 @@ const router = createBrowserRouter([
   },
   {
     path: "/learning/:questId",
-    // element: <RandomLearningLayout />, // 랜덤 레이아웃.
-    element: <LearningLayout />,
+    element: <RandomLearningLayout />, // 랜덤 레이아웃.
     children: [
       {
         path: ':stepId', // 동적 경로 파라미터 사용
@@ -56,25 +51,6 @@ const router = createBrowserRouter([
   {
     path: '/result',
     element: <GameClearPage />,
-  },
-  // 새로운 게임 페이지들
-  {
-    path: '/game/intro',
-    element: <GameIntroPage />,
-  },
-  {
-    path: "/game",
-    element: <GameLayout />,
-    children: [
-      {
-        path: ':stepId',
-        element: <GameStepPage />,
-      },
-    ],
-  },
-  {
-    path: '/game/result',
-    element: <GameResultPage />,
   },
 ]);
 
